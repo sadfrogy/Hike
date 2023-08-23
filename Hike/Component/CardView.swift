425db62a571bf7c078dd434bb6ef0ff0357131ac
Sergei <sergei.ilin1906@gmail.com>
2023-08-23 15:55:30 +0300

    Motion Animation

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -74,18 +74,7 @@ struct CardView: View {
                 // MARK: - MAIN CONTENT
                 
                 ZStack {
-                    Circle()
-                        .fill(
-                            LinearGradient(
-                                colors: [
-                                        Color("ColorIndigoMedium"),
-                                        Color("ColorSalmonLight")
-                                ],
-                                startPoint: .topLeading,
-                                endPoint: .bottomTrailing
-                            )
-                        )
-                        .frame(width: 256, height: 256)
+                    CustomCircleView()
                     
                     Image("image-\(imageNumber)")
                         .resizable()
